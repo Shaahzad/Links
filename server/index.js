908c6733368662a6fcc5bbad0b383d29a06b9eb2
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-07-04 21:06:25 +0500

    DOne

--- server/index.js
+++ server/index.js
@@ -5,22 +5,18 @@ import dotenv from "dotenv";
 import router from "./Routes/index.js";
 
 dotenv.config();
-const app = express();
-app.use(express.json());
 
-
+mongoose.connect(process.env.mongo)
+    .then(() => {
+        console.log("connected to db");
+    })
 app.use(cors({
     origin: "https://mern-crud-front-sigma.vercel.app",
     credentials: true
 }));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://mern-crud-front-sigma.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
 
+const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("server is running");
@@ -28,10 +24,6 @@ app.get("/", (req, res) => {
 app.use("/api", router)
 
 
-mongoose.connect(process.env.mongo)
-    .then(() => {
-        console.log("connected to db");
-    })
 app.listen(process.env.port, () => {
     console.log("server is running");
 });
